b2e4b53d74e58af2958513a668129eee55fe63a0
Wen Xiao <wennstyle@gmail.com>
2022-06-13 23:35:04 -0400

    commit tree 8c28b17698fbad00680f8f7090d77d4dbafd1fc7

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr.Chocolate', 12, 'M', 'brown', 'aggressive', true);
+INSERT INTO bears VALUES (2, 'Rowdy', 12, 'M', 'brown', 'aggressive', true);
+INSERT INTO bears VALUES (3, 'Tabitha', 12, 'F', 'brown', 'aggressive', true);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 12, 'M', 'brown', 'aggressive', true);
+INSERT INTO bears VALUES (5, 'Melissa', 12, 'F', 'brown', 'aggressive', true);
+INSERT INTO bears VALUES (6, 'Grinch', 12, 'M', 'brown', 'aggressive', true);
+INSERT INTO bears VALUES (7, 'Wendy', 12, 'F', 'brown', 'aggressive', true);
+INSERT INTO bears VALUES (8, NULL, 12, 'M', 'brown', 'aggressive', true);
